282a1a664c7baa6558d648d0507038c517e13eb8
Susan Johnston <Susan.Johnston@ed.ac.uk>
2018-01-17 01:38:31 +0000

    Added gradient argument to plot_fibonacci function.

--- R/plot_fibonacci.R
+++ R/plot_fibonacci.R
@@ -3,18 +3,26 @@
 #' This function creates a plot for a Fibonacci sequence of a specified length.
 #'
 #' @param len numeric. Specify the length of the sequence.
+#' @param gradient logical. Default TRUE. Indicates whether points on the plot
+#'   should be coloured by gradient.
 #' @import ggplot2
 #' @export
 
 
-plot_fibonacci <- function(len){
+plot_fibonacci <- function(len, gradient = T){
 
   require(ggplot2)
 
   x <- data.frame(Order = 1:len,
                   Fibonacci.Number = fibonacci(len))
 
-  ggplot(x, aes(Order, Fibonacci.Number, colour = Order)) +
+  if(gradient){
+    p <- ggplot(x, aes(Order, Fibonacci.Number, colour = Order))
+  } else {
+    p <- ggplot(x, aes(Order, Fibonacci.Number))
+  }
+
+  p +
     geom_point(size = 2) +
     geom_line() +
     theme_bw() +
